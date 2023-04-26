3ed07b41deca0131dbc57c715ef51fa6d9234a0f
Joshua <tjosh.owoyemi@gmail.com>
2023-04-26 22:53:03 +0900

    add more info in shown records

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 
 import pandas as pd
 import streamlit as st
+from datetime import datetime
 
 import src.backend.database as db
 from src.backend.schema import Record
@@ -31,6 +32,7 @@ st.write(
 )
 st.markdown("---")
 st.subheader("Patient Vitals Validation")
+st.write("*[Insert instructions here]*")
 
 
 @st.cache_data()
@@ -57,8 +59,7 @@ expert_name = st.text_input(
 )
 
 with st.form("entry_form", clear_on_submit=True):
-    patient_id, sample_data = get_patient_data(50)
-    print(sample_data)
+    patient_id, patient_data = get_patient_data(50)
     st.write(f"#### Patient ID: {patient_id}")
     col1, col2, col3 = st.columns(3)
     with col1:
@@ -67,26 +68,41 @@ with st.form("entry_form", clear_on_submit=True):
         st.write("**Recorded Vitals**")
     with col3:
         st.write("**Record Validity Score**")
-    for i, item in enumerate(sample_data):
+    for i, item in enumerate(patient_data):
         record = Record(**item)
         col1, col2, col3 = st.columns(3)
         with col1:
             st.write(f"{record.datetime_record_created}")
         with col2:
             st.write(f"{record.vitals}: {record.vitals_reading} {record.unit}")
+            if record.body_position:
+                st.write(f"Body Position: {record.body_position}")
+            if record.vitals_type:
+                st.write(f"Vitals Type: {record.vitals_type}")
         with col3:
             score = st.number_input(
                 f"{record.record_id} Score",
-                value=3,
+                value=4,
                 step=1,
+                max_value=5,
+                min_value=1,
                 label_visibility="hidden",
             )
+            item["expert_validity"] += [
+                {
+                    "expert_id": 1234,  # TODO: assign unique ids to experts
+                    "score": score,
+                    "created_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                }
+            ]
         st.markdown("---")
 
     submitted = st.form_submit_button(
-        "Save Data", disabled=(expert_name == "")
+        "Save Data",
+        # disabled=(expert_name == "")
     )
     if submitted:
+        print("item: ", item)
         # TODO: submit data to database
         st.cache_data.clear()
         st.experimental_rerun()
